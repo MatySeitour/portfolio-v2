e5a95a9b373f178c00205dd0364fbbe03e362c1f
Matias Seitour <matias.seitour01@gmail.com>
2024-01-03 00:15:59 -0300

    feat(nav) change style to nav items

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -5,24 +5,20 @@ import { inter } from "../assets/fonts";
 
 export default function Nav() {
   return (
-    <motion.nav
-      initial={{ top: -100 }}
-      animate={{ top: 0 }}
-      className="fixed top-0 z-50 h-14 w-full"
-    >
+    <motion.nav className="fixed top-0 z-50 h-14 w-full">
       <ul
         className={`flex h-full w-full justify-center gap-4 px-4 py-1 ${inter.className} bg-[#000a] py-2`}
       >
-        <li className="flex cursor-pointer items-center rounded-full border border-white/10 px-4 text-white/70 transition-all hover:text-white">
+        <li className="flex cursor-pointer items-center rounded-full border border-white/10 bg-gradient-nav px-4 text-white/70 transition-all hover:text-white">
           Home
         </li>
-        <li className="flex cursor-pointer items-center rounded-full  border border-white/10 px-4 text-white/70 transition-all hover:text-white">
+        <li className="flex cursor-pointer items-center rounded-full border  border-white/10 bg-gradient-nav px-4 text-white/70 transition-all hover:text-white">
           About
         </li>
-        <li className="flex cursor-pointer items-center rounded-full  border border-white/10 px-4 text-white/70 transition-all hover:text-white">
+        <li className="flex cursor-pointer items-center rounded-full border  border-white/10 bg-gradient-nav px-4 text-white/70 transition-all hover:text-white">
           Experience
         </li>
-        <li className="flex cursor-pointer items-center rounded-full  border border-white/10 px-4 text-white/70 transition-all hover:text-white">
+        <li className="flex cursor-pointer items-center rounded-full border  border-white/10 bg-gradient-nav px-4 text-white/70 transition-all hover:text-white">
           Skills
         </li>
       </ul>
