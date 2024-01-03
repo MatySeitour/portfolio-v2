8ec24811ce0f3249be6cd710c747bf9b22e7ac59
Matias Seitour <matias.seitour01@gmail.com>
2024-01-03 00:14:25 -0300

    feat(tailwind) add gradient backgrounds image

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-
+      backgroundImage: {
+        "gradient-title": "linear-gradient(to bottom, #ddd 30%, #fff 60%, #444);",
+        "gradient-nav": "linear-gradient(45deg, #222, transparent);",
+        "gradient-blue": "linear-gradient(0deg, #02f5 , transparent, transparent);",
+        "gradient-purple": "linear-gradient(0deg, #a0f5 , transparent, transparent);",
+        "gradient-orange": "linear-gradient(0deg, #f848 , transparent, transparent);"
+      },
     },
   },
   plugins: [],
