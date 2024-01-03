0e51964d5c72c7850f4c52d6d60247a6755d9319
Matias Seitour <matias.seitour01@gmail.com>
2024-01-03 00:17:15 -0300

    feat(fonts) add inter and paytone fonts

---
+++ src/app/assets/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Paytone_One } from "next/font/google";
+
+export const paytone_One = Paytone_One({
+  weight: ["400"],
+  subsets: ["latin"],
+});
+
+export const inter = Inter({
+  weight: ["300", "500", "600", "700", "800"],
+  subsets: ["latin"],
+});
