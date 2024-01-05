bb7ddfcaf90f7cbd932c4018d778650e0f340e5b
Matias Seitour <matias.seitour01@gmail.com>
2024-01-05 20:15:36 -0300

    feat(firstRing) add technologyItem props

--- src/app/components/FirstRing.tsx
+++ src/app/components/FirstRing.tsx
@@ -7,22 +7,31 @@ import { borderEffect } from "../utils/animations/animations";
 
 export default function FirstRing() {
   return (
-    <div className="absolute left-1/2 top-1/2 z-50 h-36 w-52 -translate-x-1/2 -translate-y-1/2 rounded-[50%]">
+    <div className="absolute left-1/2 top-1/2 z-50 h-40 w-56 -translate-x-1/2 -translate-y-1/2 rounded-[50%]">
       <TechnologyItem
+        gradientColor="from-html"
         name="html"
         className="-left-6 top-1/2"
-        IconComponent={FaHtml5}
+        svgImage={"html.svg"}
       />
       <TechnologyItem
         name="css"
+        gradientColor="from-css"
         className="-right-4 top-1/2"
-        IconComponent={FaCss3Alt}
+        svgImage={"css.svg"}
       />
       <TechnologyItem
+        gradientColor="from-javascript"
         name="javascript"
         className="left-1/2 top-0 -translate-x-1/2 -translate-y-1/2"
-        IconComponent={FaJs}
+        svgImage={"js.svg"}
       />
+      {/* <TechnologyItem
+        colorIcon="ffa"
+        name=""
+        className="left-1/2 top-1/2 -translate-x-1/2 -translate-y-6"
+        svgImage={FaJs}
+      /> */}
       <motion.div
         initial={borderEffect.rest}
         animate={borderEffect.hover}
