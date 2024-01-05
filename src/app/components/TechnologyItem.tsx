1a3123be8825ed1cde2482aada0bdf598e3c6d9a
Matias Seitour <matias.seitour01@gmail.com>
2024-01-05 20:14:43 -0300

    feat(technologyItem) add svgImage and gradient color props to gradient icon

--- src/app/components/TechnologyItem.tsx
+++ src/app/components/TechnologyItem.tsx
@@ -1,25 +1,72 @@
 "use client";
 
 import { useEffect } from "react";
+import { motion } from "framer-motion";
+import Image from "next/image";
 
 export default function TechnologyItem({
-  IconComponent,
+  svgImage,
   className,
   name,
+  gradientColor,
 }: {
-  IconComponent: React.ComponentType<{ className?: string }>;
+  svgImage: string;
   className: string;
   name: string;
+  gradientColor: string;
 }) {
+  const hoverScale = {
+    rest: {
+      scale: 1,
+    },
+    hover: {
+      scale: 2,
+      opacity: 0,
+      transition: {
+        duration: 2,
+        repeat: Infinity,
+      },
+    },
+  };
+
+  // useEffect(() => {
+  //   const itemsSkill = document.querySelectorAll(".skill-item");
+  //   const cursor = document.querySelector(".cursor");
+
+  //   itemsSkill.forEach((item: any) => {
+  //     item.addEventListener("mousemove", (e: any) => {
+  //       cursor?.classList.add("bg-edit");
+  //     });
+  //     item.addEventListener("mouseleave", (e: any) => {
+  //       cursor?.classList.remove("bg-edit");
+  //     });
+  //   });
+  // }, []);
+
   return (
     <div
-      className={`skill-item absolute z-10 flex h-16 w-16 flex-col items-center justify-center rounded-[50%] ${
+      className={`skill-item w-2h-20 absolute z-10 flex h-20 flex-col items-center justify-center rounded-[50%] ${
         className && className
       }`}
     >
-      <div className="relative h-12 w-12 rounded-[50%] bg-black bg-gradient-nav p-[10px]">
-        <IconComponent className="h-full w-full text-white"></IconComponent>
-        <div className="effect-radiant radiant-gray"></div>
+      <div
+        // whileHover="hover"
+        // animate="rest"
+        className={`icon-skill bg-gradient-to-45 ${gradientColor} relative h-14 w-14 rounded-[50%] bg-black p-[10px]`}
+      >
+        {/* <IconComponent className={`h-full w-full ${colorIcon}`}></IconComponent> */}
+        <Image
+          src={`/${svgImage}`}
+          alt={name}
+          className="h-full w-full"
+          width={400}
+          height={400}
+        />
+        <div className="effect-radiant radiant-gray !pb-[0px] !pl-[0px]"></div>
+        <span
+          // variants={hoverScale}
+          className="absolute left-0 top-0 -z-10 h-full w-full rounded-[50%] bg-[#f846]"
+        ></span>
       </div>
       <div className="radial-effect__bg absolute left-0 top-0 -z-10 h-full w-full"></div>
       <p className="text-white">{name}</p>
