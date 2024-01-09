ac52fece0f9cc3759aa66a5d131e29a0b1b5baed
Matias Seitour <matias.seitour01@gmail.com>
2024-01-08 23:56:22 -0300

    feat(secondRing) add svg technology item svg and gradient color prop

--- src/app/components/SecondRing.tsx
+++ src/app/components/SecondRing.tsx
@@ -6,25 +6,46 @@ import TechnologyItem from "./TechnologyItem";
 
 export default function SecondRing() {
   return (
-    <div className="absolute left-1/2 top-1/2 z-10 h-80 w-96 -translate-x-1/2 -translate-y-1/2 rounded-[50%] border border-white/20">
+    <div className="absolute left-1/2 top-1/2 z-10 h-[28rem] w-[34rem] -translate-x-1/2 -translate-y-1/2 rounded-[50%] border border-white/20">
       <TechnologyItem
         gradientColor="from-react"
-        svgImage={"react.svg"}
-        className="left-6 top-5"
-        name="react"
+        svgImage={"mysql.svg"}
+        className="-left-6 top-1/2 -translate-y-1/2"
+        name="mysql"
       />
       <TechnologyItem
-        gradientColor="from-typescript"
-        svgImage={"typescript.svg"}
-        className="-bottom-10 right-1/2 translate-x-1/2"
-        name="typescript"
+        gradientColor="from-postgresql"
+        svgImage={"postgresql.svg"}
+        className="-right-10 top-1/2 -translate-y-1/2"
+        name="postgresql"
       />
       <TechnologyItem
-        gradientColor="from-next"
-        svgImage={"nextjs.svg"}
-        className="right-6 top-5"
-        name="nextjs"
+        gradientColor="from-golang"
+        svgImage={"golang.svg"}
+        className="-bottom-10 left-12 -translate-y-1/2 translate-x-1/2 "
+        name="golang"
       />
+      <TechnologyItem
+        gradientColor="from-node"
+        svgImage={"node.svg"}
+        className="left-24 top-16 -translate-x-1/2 -translate-y-1/2 "
+        name="nodejs"
+      />
+      <TechnologyItem
+        gradientColor="from-express"
+        svgImage={"express.svg"}
+        className="right-10 top-16 -translate-x-1/2 -translate-y-1/2 "
+        name="express"
+      />
+      <TechnologyItem
+        gradientColor="from-docker"
+        svgImage={"docker.svg"}
+        className="-bottom-10 right-12 -translate-x-1/2 -translate-y-1/2 "
+        name="docker"
+      />
+      <div className="absolute left-1/2 top-10 -translate-x-1/2 -translate-y-1/2 bg-gradient-title bg-clip-text text-2xl font-bold text-transparent">
+        Backend
+      </div>
     </div>
   );
 }
