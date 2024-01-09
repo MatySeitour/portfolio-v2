a29b85ffc6fe5efc838b814cf659044da3f34357
Matias Seitour <matias.seitour01@gmail.com>
2024-01-08 23:57:25 -0300

    refactor(iluminationEffect) delete useless code

--- src/app/components/IluminationEffect.tsx
+++ src/app/components/IluminationEffect.tsx
@@ -1,29 +1,11 @@
-import { motion } from "framer-motion";
-
 export default function IluminationEffect({
   className,
 }: {
   className?: string;
 }) {
-  const onboardingCardAnimation = {
-    hidden: { opacity: 0 },
-    visible: {
-      opacity: 1,
-      transition: {
-        ease: [0.7, 0.8, 0.6, 1],
-        duration: 0.5,
-      },
-    },
-  };
-
   return (
     <>
-      <motion.span
-        initial="hidden"
-        animate="visible"
-        variants={onboardingCardAnimation}
-        className={`banner ${className && className}`}
-      ></motion.span>
+      <span className={`banner ${className && className}`}></span>
     </>
   );
 }
