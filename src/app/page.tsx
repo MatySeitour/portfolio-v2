e8bfac5717b84e9038f947b375949b46d979b3ec
Matias Seitour <matias.seitour01@gmail.com>
2024-01-10 23:22:02 -0300

    feat(page) add divider and background dark

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ import { inter } from "./assets/fonts";
 import Experience from "./components/Experience";
 import { useEffect } from "react";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import IluminationEffect from "./components/IluminationEffect";
+import Divider from "./components/Divider";
 
 export default function Home() {
   return (
@@ -12,13 +15,18 @@ export default function Home() {
       className={`overflow-hidden ${inter.className} flex flex-col items-center justify-center`}
     >
       <Nav />
-      <div className="fixed top-0 h-full w-full"></div>
+      <div className="fixed top-0 -z-10 h-full w-full bg-[#000a]"></div>
+
       <div className="w-full max-w-7xl">
         <Banner />
+        <Divider />
+        <Experience />
+        <Divider />
+        <Skills />
+        <Divider />
+        <Projects />
       </div>
-      <Experience />
       <div className="cursor"></div>
-      <Skills />
     </main>
   );
 }
