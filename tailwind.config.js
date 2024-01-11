739336fdeb3bf65ec147a1f5cc16ed73a751614c
Matias Seitour <matias.seitour01@gmail.com>
2024-01-10 23:18:55 -0300

    feat(tailwind) add gradient-icon style and change some colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,7 @@ module.exports = {
         "argentina": "linear-gradient(to top, #0ff, #fff, #0ff)",
         "gradient-to-45": "linear-gradient(45deg, var(--tw-gradient-stops), transparent)",
         "gradient-title": "linear-gradient(to bottom, #ddd 30%, #fff 60%, #444);",
+        "gradient-icon": "linear-gradient(var(--tw-gradient-stops) 30%, var(--tw-gradient-stops));",
         "gradient-nav": "linear-gradient(45deg, #222, transparent);",
         "gradient-blue": "linear-gradient(0deg, #02f5 , transparent, transparent);",
         "gradient-purple": "linear-gradient(0deg, #a0f5 , transparent, transparent);",
@@ -36,18 +37,18 @@ module.exports = {
         "gradient-blue-blur": "linear-gradient(#02f8 30%, #02f5);"
       },
       colors: {
-        "html": "#f846",
+        "html": "#f84f",
         "css": "#00a7ff",
-        "javascript": "#ff06",
+        "javascript": "#ff0f",
         "react": "#09ff",
-        "typescript": "#07fa",
-        "next": "#555",
-        "node": "#0a7a",
-        "express": "#777a",
+        "typescript": "#07ff",
+        "next": "#ccc",
+        "node": "#0a7f",
+        "express": "#fffa",
         "postgresql": "#0cfa",
         "docker": "#0afa",
         "golang": "#0cfa",
-
+        "prisma": "#0faf",
       }
     },
   },
