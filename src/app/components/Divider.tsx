e0970f75154d38d47df44016f4b1533cf4928b5c
Matias Seitour <matias.seitour01@gmail.com>
2024-01-10 23:19:23 -0300

    feat(divider) create divider component

--- src/app/components/Divider.tsx
+++ src/app/components/Divider.tsx
@@ -1,8 +1,8 @@
 export default function Divider() {
   return (
     <div className="relative h-40 pt-20">
-      <div className="border-effect__a absolute bottom-0 right-1/2 h-[1.5px] w-full translate-x-1/2">
-        <div className="banner radiant-a absolute -top-1/2 left-1/2 h-32 w-32 -translate-x-1/2 -translate-y-1/2 rounded-full"></div>
+      <div className="border-effect__divider absolute bottom-0 right-1/2 h-[1.5px] w-full translate-x-1/2">
+        <div className="banner radiant-divider absolute -top-1/2 left-1/2 h-32 w-32 -translate-x-1/2 -translate-y-1/2 rounded-full"></div>
       </div>
     </div>
   );
