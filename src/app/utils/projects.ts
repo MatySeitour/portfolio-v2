b9fce6cd339fcd48d22bbf1d6cbc286013a3eeb3
Matias Seitour <matias.seitour01@gmail.com>
2024-01-10 23:23:13 -0300

    feat(utils/projects) add projects data

---
+++ src/app/utils/projects.ts
@@ -0,0 +1,17 @@
+interface Project {
+  projectName: string;
+  projectDescription: string;
+  technologies: Array<string>;
+  demoLink?: string;
+  githubLink: string;
+}
+
+export const projects: Project[] = [
+  {
+    projectName: "Que me están cobrando",
+    projectDescription: "lalal ad s ome sdalkelk dasd",
+    technologies: ["nextjs, typescript, prisma, puppeteer, gsap"],
+    demoLink: "http://",
+    githubLink: "http://",
+  },
+];
