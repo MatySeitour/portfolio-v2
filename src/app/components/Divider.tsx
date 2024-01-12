44a97d7e7fb2ae55302edb35a235484dd3b3873b
Matias Seitour <matias.seitour01@gmail.com>
2024-01-11 23:53:31 -0300

    feat(divider) add appear animation

--- src/app/components/Divider.tsx
+++ src/app/components/Divider.tsx
@@ -1,9 +1,26 @@
+"use client";
+
+import { motion } from "framer-motion";
+import {
+  appear,
+  containerObserverAnimation,
+} from "../utils/animations/animations";
+
 export default function Divider() {
   return (
-    <div className="relative h-40 pt-20">
-      <div className="border-effect__divider absolute bottom-0 right-1/2 h-[1.5px] w-full translate-x-1/2">
+    <motion.div
+      initial="offscreen"
+      whileInView="onscreen"
+      variants={containerObserverAnimation}
+      viewport={{ once: true, amount: 0.5 }}
+      className="relative h-40 pt-20"
+    >
+      <motion.div
+        variants={appear}
+        className="border-effect__divider absolute bottom-0 right-1/2 h-[1.5px] w-full translate-x-1/2"
+      >
         <div className="banner radiant-divider absolute -top-1/2 left-1/2 h-32 w-32 -translate-x-1/2 -translate-y-1/2 rounded-full"></div>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 }
