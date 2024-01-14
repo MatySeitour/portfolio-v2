154ac14a74ccdf3d97e67335c57a2ba3195bed4a
Matias Seitour <matias.seitour01@gmail.com>
2024-01-13 23:44:48 -0300

    refactor(utils/contact) delete useless code

--- src/app/utils/contact.ts
+++ src/app/utils/contact.ts
@@ -1,18 +1,15 @@
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
-import { SiGmail } from "react-icons/si";
 
 export const contactItems = [
   {
     name: "GitHub",
     link: "https://github.com/MatySeitour",
-    // color: "#fff",
     image: FaGithub,
   },
   {
     name: "LinkedIn",
     link: "https://www.linkedin.com/in/matias-seitour-1a0118240/",
-    // color: "#00a",
     image: FaLinkedin,
   },
 ];
