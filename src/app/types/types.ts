49ea755b54886436d4ad50594b37c9af223cc7d9
Matias Seitour <matias.seitour01@gmail.com>
2024-01-13 23:45:26 -0300

    refactor(types) add a single file for types

--- src/app/types/types.ts
+++ src/app/types/types.ts
@@ -1,10 +1,19 @@
-export type ContactType = {
+export type Contact = {
   name: string;
   link: string;
   image: React.ComponentType<{
     className?: string;
   }>;
 };
+
+export interface Project {
+  projectName: string;
+  projectDescription: string;
+  technologies: Array<string>;
+  imageSrc: string;
+  demoLink?: string;
+  githubLink: string;
+}
 
 // {
 //     name: "GitHub",
