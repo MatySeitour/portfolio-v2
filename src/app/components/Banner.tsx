f074eb908e6877a08b4e1ba70cb371a702d45722
Matias Seitour <matias.seitour01@gmail.com>
2024-01-14 23:16:20 -0300

    feat(banner) delete useless code

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -1,11 +1,25 @@
 "use client";
 
-import { inter } from "../assets/fonts";
+// components ----------------
 import ContactBanner from "./ContactBanner";
-import { motion } from "framer-motion";
 import IluminationEffect from "./IluminationEffect";
-import { bgContactGitHub, borderEffect } from "../utils/animations/animations";
+import { motion } from "framer-motion";
+
+// hooks ----------------
 import { useEffect, useState } from "react";
+
+// fonts ----------------
+import { inter } from "../assets/fonts";
+
+// animations ----------------
+import {
+  bannerContainerAnimation,
+  borderEffect,
+  subtitleBannerAnimation,
+  titleBannerAnimation,
+} from "../utils/animations/animations";
+
+// icons ----------------
 import { FaCopy } from "react-icons/fa";
 import { FaCheck } from "react-icons/fa6";
 
@@ -14,7 +28,6 @@ export default function Banner() {
 
   useEffect(() => {
     const cursor = document.querySelector(".cursor");
-
     document.addEventListener("mousemove", function (e: any) {
       const posX = e.clientX;
       const posY = e.clientY;
@@ -41,68 +54,33 @@ export default function Banner() {
     }, 1000);
   };
 
-  const onboardingContainerAnimation = {
-    visible: {
-      transition: {
-        ease: [0, 0.71, 0.2, 1.01],
-        staggerChildren: 0.2,
-        duration: 0.5,
-      },
-    },
-  };
-
-  const titleBannerAnimation = {
-    hidden: { opacity: 0, translateY: -50 },
-    visible: {
-      translateY: 0,
-      opacity: 1,
-      transition: {
-        type: "spring",
-        bounce: 0.4,
-        duration: 0.5,
-      },
-    },
-  };
-  const subtitleBannerAnimation = {
-    hidden: { opacity: 0, translateY: 50 },
-    visible: {
-      translateY: 0,
-      opacity: 1,
-      transition: {
-        type: "spring",
-        bounce: 0.4,
-        duration: 0.5,
-      },
-    },
-  };
-
   return (
     <motion.div
       initial="hidden"
       animate="visible"
-      variants={onboardingContainerAnimation}
-      className="relative flex h-full flex-row items-center justify-between gap-8 bg-[#000a] px-8 pt-40"
+      variants={bannerContainerAnimation}
+      className="relative flex h-full flex-col-reverse items-center justify-between gap-8 bg-[#000a] px-8 pt-40 md:flex-row"
     >
-      <IluminationEffect className="absolute right-0 top-0 h-[26rem] w-[50rem]  translate-x-1/2  rotate-[200deg] rounded-full" />
+      <IluminationEffect className="absolute -top-1/2 left-1/2 h-[26rem] w-[26rem] -translate-x-1/2  rotate-[200deg] rounded-full" />
 
       <motion.div className="relative flex flex-col gap-8">
         <div className="flex flex-col gap-4">
           <motion.h1
             variants={titleBannerAnimation}
-            className="w-auto overflow-hidden bg-gradient-title bg-clip-text text-7xl font-semibold text-transparent"
+            className="w-auto overflow-hidden bg-gradient-title bg-clip-text text-center text-4xl font-semibold text-transparent md:text-left md:text-7xl"
           >
             Matias Seitour
           </motion.h1>
-          <div className="flex items-center justify-start gap-2 text-2xl">
+          <div className="flex items-center justify-start gap-2 text-center text-lg md:text-left md:text-2xl">
             <motion.h2
               variants={subtitleBannerAnimation}
-              className={`w-auto overflow-hidden ${inter.className} text-banner text-white/40`}
+              className={`w-auto overflow-hidden ${inter.className} text-banner text-center text-white/40 md:text-left`}
             >
               <b className="text-white">FullStack developer</b>
               {" (or so i think) from  "}
               <motion.b
                 variants={subtitleBannerAnimation}
-                className={`${inter.className} w-auto overflow-hidden bg-argentina bg-clip-text text-transparent`}
+                className={`${inter.className} w-auto overflow-hidden bg-argentina bg-clip-text text-center text-transparent md:text-left`}
               >
                 Argentina
               </motion.b>
@@ -118,24 +96,14 @@ export default function Banner() {
           variants={subtitleBannerAnimation}
           className="flex max-w-xs items-center gap-4"
         >
-          <div className="relative rounded-lg px-4 py-2 text-white">
+          <div className="relative rounded-lg bg-gradient-project px-4 py-2 text-white">
             <p className="bg-re">matias.seitour01@gmail.com</p>
             <div className="border-effect__bottom absolute bottom-[0px] right-1/2 h-[2px] w-full translate-x-1/2"></div>
             <div className="effect-radiant radiant-gray"></div>
           </div>
-          <motion.div
-            initial={{
-              backgroundImage: "linear-gradient(45deg, #000,  transparent)",
-            }}
-            whileHover={{
-              backgroundImage: bgContactGitHub,
-              transition: {
-                duration: 0.5,
-                type: "spring",
-              },
-            }}
+          <div
             onClick={handleClick}
-            className="relative cursor-pointer rounded-lg px-4 py-[8.5px] text-white"
+            className="relative z-30 cursor-pointer rounded-lg bg-gradient-button px-4 py-[8.5px] text-white"
           >
             <FaCopy
               className={`absolute z-10 h-6 w-6 ${
@@ -149,14 +117,14 @@ export default function Banner() {
               } text-green-600 transition-all`}
             />
             <div className="effect-radiant radiant-gray"></div>
-          </motion.div>
+          </div>
         </motion.div>
       </motion.div>
       <motion.div
         initial="rest"
         whileHover="hover"
         animate="rest"
-        className="group-data relative h-96 w-96 rounded-lg bg-black "
+        className="group-data relative h-72 w-72 rounded-lg bg-black md:h-96 md:w-96 "
       >
         <motion.div
           variants={borderEffect}
