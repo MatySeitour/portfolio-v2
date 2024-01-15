0704f993ae51f8a799c230ab9c7f85aeb7c84f0f
Matias Seitour <matias.seitour01@gmail.com>
2024-01-14 23:20:42 -0300

    feat(technologyItem) add shadow effect and gradient color items

--- src/app/components/TechnologyItem.tsx
+++ src/app/components/TechnologyItem.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { useEffect } from "react";
-import { motion } from "framer-motion";
 import Image from "next/image";
 
 export default function TechnologyItem({
@@ -15,46 +13,15 @@ export default function TechnologyItem({
   name: string;
   gradientColor: string;
 }) {
-  const hoverScale = {
-    rest: {
-      scale: 1,
-    },
-    hover: {
-      scale: 2,
-      opacity: 0,
-      transition: {
-        duration: 2,
-        repeat: Infinity,
-      },
-    },
-  };
-
-  // useEffect(() => {
-  //   const itemsSkill = document.querySelectorAll(".skill-item");
-  //   const cursor = document.querySelector(".cursor");
-
-  //   itemsSkill.forEach((item: any) => {
-  //     item.addEventListener("mousemove", (e: any) => {
-  //       cursor?.classList.add("bg-edit");
-  //     });
-  //     item.addEventListener("mouseleave", (e: any) => {
-  //       cursor?.classList.remove("bg-edit");
-  //     });
-  //   });
-  // }, []);
-
   return (
     <div
-      className={`skill-item w-2h-20 absolute z-10 flex h-20 flex-col items-center justify-center rounded-[50%] ${
+      className={`absolute z-10 flex h-20 flex-col items-center justify-center rounded-[50%] ${
         className && className
       }`}
     >
       <div
-        // whileHover="hover"
-        // animate="rest"
-        className={`bg-gradient-to-45 hover:animate-skill__${name} ${gradientColor} relative h-14 w-14 rounded-[50%] bg-black p-[10px]`}
+        className={`bg-gradient-to-45  ${gradientColor} relative h-14 w-14 rounded-[50%] bg-black p-[10px]`}
       >
-        {/* <IconComponent className={`h-full w-full ${colorIcon}`}></IconComponent> */}
         <Image
           src={`/${svgImage}`}
           alt={name}
@@ -63,11 +30,11 @@ export default function TechnologyItem({
           height={400}
         />
         <div className="effect-radiant radiant-gray !pb-[0px] !pl-[0px]"></div>
+        <div
+          className={`shadow-effect__10 absolute left-0 top-0 -z-10 h-full w-full rounded-[50%] bg-gradient-icon  transition-all  from-${gradientColor} to-${gradientColor}`}
+        ></div>
       </div>
-      <div className="absolute left-0 top-0 -z-10 h-full w-full"></div>
       <p className="text-white">{name}</p>
     </div>
   );
 }
-
-// -left-6 top-1/2 z-10 flex h-16 w-16
