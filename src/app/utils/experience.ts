2b370069f402b0afa285f0e1d029e294dd3d76ab
Matias Seitour <matias.seitour01@gmail.com>
2024-01-16 23:45:17 -0300

    feat(utils/experience) add experience items

---
+++ src/app/utils/experience.ts
@@ -0,0 +1,26 @@
+export const experienceItems = [
+  {
+    title: "Frontend Developer at DPSoftware",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit Commodi excepturi maxime maiores amet harum earum saepeid odio nesciunt! Eius pariatur ipsum officia estlaudantium culpa labore itaque adipisci animi",
+    year: "2023",
+  },
+  {
+    title: "Maintenance and development on gta roleplay server",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit Commodi excepturi maxime maiores amet harum earum saepeid odio nesciunt! Eius pariatur ipsum officia estlaudantium culpa labore itaque adipisci animi",
+    year: "2023",
+  },
+  {
+    title: "laslda",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit Commodi excepturi maxime maiores amet harum earum saepeid odio nesciunt! Eius pariatur ipsum officia estlaudantium culpa labore itaque adipisci animi",
+    year: "2023",
+  },
+  {
+    title: "Mgta roleplay server",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit Commodi excepturi maxime maiores amet harum earum saepeid odio nesciunt! Eius pariatur ipsum officia estlaudantium culpa labore itaque adipisci animi",
+    year: "2023",
+  },
+];
