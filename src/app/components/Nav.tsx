58485ec1c0121eaee0a7a6b77fff33fa41b89285
Matias Seitour <matias.seitour01@gmail.com>
2024-01-18 23:55:29 -0300

    feat(nav) add responsive mobile

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -5,19 +5,19 @@ import { inter } from "../assets/fonts";
 
 export default function Nav() {
   return (
-    <motion.nav className="fixed top-4 z-50 h-14 w-full">
+    <motion.nav className="fixed top-4 z-50 h-14 w-full md:h-14">
       <div className={`flex h-full w-auto justify-center ${inter.className}`}>
-        <ul className="bg-gradient-project relative flex gap-4 rounded-full bg-[#000a] px-2 py-1 backdrop-blur-sm">
-          <li className="relative z-10 flex cursor-pointer items-center rounded-full px-3 text-white/70 transition-all hover:text-white">
+        <ul className="relative flex gap-4 rounded-full bg-[#000a] bg-gradient-project px-4 py-1 backdrop-blur-sm md:px-2">
+          <li className="relative z-10 flex cursor-pointer items-center rounded-full px-1 text-white/70 transition-all hover:text-white md:px-3">
             Home
           </li>
-          <li className="relative z-10 flex cursor-pointer items-center rounded-full px-3 text-white/70 transition-all hover:text-white">
+          <li className="relative z-10 flex cursor-pointer items-center rounded-full px-1 text-white/70 transition-all hover:text-white md:px-3">
             About
           </li>
-          <li className="relative z-10 flex cursor-pointer items-center rounded-full px-3 text-white/70 transition-all hover:text-white">
+          <li className="relative z-10 flex cursor-pointer items-center rounded-full px-1 text-white/70 transition-all hover:text-white md:px-3">
             Experience
           </li>
-          <li className="relative z-10 flex cursor-pointer items-center rounded-full px-3 text-white/70 transition-all hover:text-white">
+          <li className="relative z-10 flex cursor-pointer items-center rounded-full px-1 text-white/70 transition-all hover:text-white md:px-3">
             Skills
           </li>
           <motion.div className="effect-radiant radiant-gray"></motion.div>
