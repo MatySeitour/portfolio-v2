39f98cf2738dc2cadeb803ade28d1847bd1dbdf8
Matias Seitour <matias.seitour01@gmail.com>
2024-02-08 20:32:29 -0300

    refactor(nav) add div parent

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -7,21 +7,23 @@ export default function Nav() {
   return (
     <motion.nav className="fixed top-4 z-50 h-14 w-full md:h-14">
       <div className={`flex h-full w-auto justify-center ${inter.className}`}>
-        <ul className="relative flex gap-4 rounded-lg bg-[#000a] bg-gradient-bottom from-[#333] px-4 py-1 backdrop-blur-sm md:px-2">
-          <li className="relative z-10 flex cursor-pointer items-center rounded-full px-1 text-white/70 transition-all hover:text-white md:px-3">
-            Home
-          </li>
-          <li className="relative z-10 flex cursor-pointer items-center rounded-full px-1 text-white/70 transition-all hover:text-white md:px-3">
-            About
-          </li>
-          <li className="relative z-10 flex cursor-pointer items-center rounded-full px-1 text-white/70 transition-all hover:text-white md:px-3">
-            Experience
-          </li>
-          <li className="relative z-10 flex cursor-pointer items-center rounded-full px-1 text-white/70 transition-all hover:text-white md:px-3">
-            Skills
-          </li>
-          <motion.div className="effect-radiant radiant-gray"></motion.div>
-        </ul>
+        <div className="relative flex h-full rounded-lg bg-[#000a] bg-gradient-bottom from-[#333] backdrop-blur-sm md:px-2">
+          <ul className="flex gap-4 px-2 py-1">
+            <li className="relative z-20 flex cursor-pointer items-center rounded-full px-1 text-white/70 transition-all hover:text-white md:px-3">
+              Home
+            </li>
+            <li className="relative z-20 flex cursor-pointer items-center rounded-full px-1 text-white/70 transition-all hover:text-white md:px-3">
+              About
+            </li>
+            <li className="relative z-20 flex cursor-pointer items-center rounded-full px-1 text-white/70 transition-all hover:text-white md:px-3">
+              Experience
+            </li>
+            <li className="relative z-20 flex cursor-pointer items-center rounded-full px-1 text-white/70 transition-all hover:text-white md:px-3">
+              Skills
+            </li>
+          </ul>
+          <motion.div className="effect-radiant radiant-gray z-10"></motion.div>
+        </div>
       </div>
     </motion.nav>
   );
