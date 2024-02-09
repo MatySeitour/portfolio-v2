d56516985192bd30ddcce96268cc5bc8fb26c8ec
Matias Seitour <matias.seitour01@gmail.com>
2024-02-09 18:05:59 -0300

    feat(techonologyProject) add effect border radiant

--- src/app/components/TechnologyProject.tsx
+++ src/app/components/TechnologyProject.tsx
@@ -6,15 +6,16 @@ export default function ThecnologyProject({
   technologyName: string;
 }) {
   return (
-    <li className="flex h-9 w-auto items-center justify-center gap-1 rounded-md border border-gray-500 px-1 pr-2">
+    <li className="relative flex h-9 w-auto items-center justify-center gap-1 rounded-md bg-gradient-button px-2 pr-2">
       <Image
         alt={technologyName}
         src={`/${technologyName}.svg`}
         width={500}
         height={500}
-        className="h-6 w-6"
+        className="h-5 w-5"
       />
-      <p className="text-white">{technologyName}</p>
+      <p className="text-sm text-white">{technologyName}</p>
+      <div className="effect-radiant radiant-gray z-10 !rounded-lg bg-black !pl-[0.8px]"></div>
     </li>
   );
 }
