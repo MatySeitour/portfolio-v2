13a60c9d9c82edd8a5dca55dd3412cdd527e69be
Matias Seitour <matias.seitour01@gmail.com>
2024-02-09 18:28:17 -0300

    feat(nav) add cursor pointer to nav items

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -5,21 +5,29 @@ import { inter } from "../assets/fonts";
 
 export default function Nav() {
   return (
-    <motion.nav className="fixed top-4 z-50 h-14 w-full md:h-14">
+    <motion.nav className="fixed top-4 z-50 h-14 w-full md:h-12">
       <div className={`flex h-full w-auto justify-center ${inter.className}`}>
         <div className="relative flex h-full rounded-lg bg-[#000a] bg-gradient-bottom from-[#333] backdrop-blur-sm md:px-2">
           <ul className="flex gap-4 px-2 py-1">
-            <li className="relative z-20 flex cursor-pointer items-center rounded-full px-1 text-white/70 transition-all hover:text-white md:px-3">
-              <a href="#home">Home</a>
+            <li className="relative z-20 flex items-center rounded-full px-1 text-white/70 transition-all hover:text-white md:px-3">
+              <a className="cursor-pointer" href="#home">
+                Home
+              </a>
             </li>
-            <li className="relative z-20 flex cursor-pointer items-center rounded-full px-1 text-white/70 transition-all hover:text-white md:px-3">
-              <a href="#about">About</a>
+            <li className="relative z-20 flex items-center rounded-full px-1 text-white/70 transition-all hover:text-white md:px-3">
+              <a className="cursor-pointer" href="#about">
+                About
+              </a>
             </li>
-            <li className="relative z-20 flex cursor-pointer items-center rounded-full px-1 text-white/70 transition-all hover:text-white md:px-3">
-              <a href="#projects">Projects</a>
+            <li className="relative z-20 flex items-center rounded-full px-1 text-white/70 transition-all hover:text-white md:px-3">
+              <a className="cursor-pointer" href="#projects">
+                Projects
+              </a>
             </li>
-            <li className="relative z-20 flex cursor-pointer items-center rounded-full px-1 text-white/70 transition-all hover:text-white md:px-3">
-              <a href="#skills">Skills</a>
+            <li className="relative z-20 flex items-center rounded-full px-1 text-white/70 transition-all hover:text-white md:px-3">
+              <a className="cursor-pointer" href="#skills">
+                Skills
+              </a>
             </li>
           </ul>
           <motion.div className="effect-radiant radiant-gray z-10"></motion.div>
