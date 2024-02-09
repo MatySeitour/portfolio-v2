9a6d9e11bab4a00e8059ef0a1ea55c96fc489b67
Matias Seitour <matias.seitour01@gmail.com>
2024-02-09 18:27:43 -0300

    feat(projectCard) change card structure

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -2,14 +2,12 @@
 
 import { motion } from "framer-motion";
 import Link from "next/link";
-import { FaExternalLinkAlt, FaGithub } from "react-icons/fa";
 import ThecnologyProject from "./TechnologyProject";
 import { Project } from "../types/types";
 import Image from "next/image";
 import {
   appear,
   containerObserverAnimation,
-  projectAnimationContainer,
 } from "../utils/animations/animations";
 import IluminationEffect from "./IluminationEffect";
 
@@ -34,35 +32,37 @@ export default function ProjectCard({ projectData }: { projectData: Project }) {
             height={768}
             className="relative h-full w-full rounded-t-md object-cover"
           ></Image>
-          <motion.div className="relative flex h-full flex-col justify-center gap-4 overflow-hidden rounded-b-md bg-[#111a] p-4">
-            <div className="relative z-20 flex items-center justify-between gap-2">
-              <Link
-                target="_blank"
-                href={projectData.demoLink}
-                className="inline-block bg-gradient-title bg-clip-text text-left text-2xl font-semibold text-transparent sm:hidden"
-              >
-                {projectData.projectName}
-              </Link>
-              <p className="hidden bg-gradient-title bg-clip-text text-left text-2xl font-semibold text-transparent sm:inline-block">
-                {projectData.projectName}
-              </p>
-              <div className="group relative hidden overflow-hidden rounded-md border border-white/20 sm:inline-block sm:p-2">
+          <motion.div className="relative flex h-full flex-col justify-between overflow-hidden rounded-b-md bg-[#111a] p-4">
+            <div className="relative z-20 flex flex-col gap-4">
+              <div className="flex w-full items-center justify-between">
                 <Link
                   target="_blank"
                   href={projectData.demoLink}
-                  className="inline-block h-full w-full bg-gradient-title bg-clip-text text-center font-bold text-transparent"
+                  className="inline-block bg-gradient-title bg-clip-text text-left text-2xl font-semibold text-transparent sm:hidden"
                 >
-                  View demo
+                  {projectData.projectName}
                 </Link>
-                <IluminationEffect className="shadow-effect__7 absolute -left-5 top-0 -z-10 h-full w-2 !bg-gradient-to-r !from-white/10 !to-white/10 transition-all group-hover:animate-iluminateEffect" />
+                <p className="hidden bg-gradient-title bg-clip-text text-left text-2xl font-semibold text-transparent sm:inline-block">
+                  {projectData.projectName}
+                </p>
+                <div className="group relative hidden overflow-hidden rounded-md border border-white/20 sm:inline-block sm:p-2">
+                  <Link
+                    target="_blank"
+                    href={projectData.demoLink}
+                    className="inline-block h-full w-full bg-gradient-title bg-clip-text text-center font-bold text-transparent"
+                  >
+                    View demo
+                  </Link>
+                  <IluminationEffect className="shadow-effect__7 absolute -left-5 top-0 -z-10 h-full w-2 !bg-gradient-to-r !from-white/10 !to-white/10 transition-all group-hover:animate-iluminateEffect" />
+                </div>
               </div>
+              <p className="pr-2 text-[0.9rem] text-gray-100">
+                {projectData.projectDescription}
+              </p>
             </div>
 
-            <p className="text-sm text-gray-100">
-              {projectData.projectDescription}
-            </p>
-            <div className="flex justify-between">
-              <ul className="relative z-10 flex h-auto w-full max-w-full flex-wrap items-center justify-start gap-2 sm:max-w-md">
+            <div className="flex max-w-xs justify-between md:max-w-full">
+              <ul className="relative z-10 flex h-auto w-full max-w-full flex-wrap items-center justify-start gap-2">
                 {projectData.technologies.map((technology) => (
                   <ThecnologyProject
                     key={technology}
