ccf60979806822215888c44a6f3bc9873381e831
Matias Seitour <matias.seitour01@gmail.com>
2024-02-14 20:56:11 -0300

    feat(skills) add appear animations to skills section and add current technology

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -3,11 +3,15 @@
 import {
   appear,
   containerObserverAnimation,
+  experienceLineContainer,
+  experienceLines,
+  experienceLinesReverse,
 } from "../utils/animations/animations";
 import FirstRing from "./FirstRing";
 import IluminationEffect from "./IluminationEffect";
 import SecondRing from "./SecondRing";
 import { motion } from "framer-motion";
+import TechnologyItem from "./TechnologyItem";
 
 export default function Skills() {
   return (
@@ -24,80 +28,61 @@ export default function Skills() {
       >
         <div className="top-30 absolute left-1/2 z-0 h-96 w-full max-w-3xl -translate-x-1/2 scale-150 scale-x-[2] rounded-t-full bg-black">
           <div className="effect-radiant radiant-skills !pb-0 !pl-0 !pr-0"></div>
-          {/* <div className="lines-left bg-gradient-to-b from-transparent !pb-[1.8px] !pl-0 !pr-[0px]">
-
-              </div> */}
         </div>
         <div className="shadow-effect__10 top-30 absolute left-1/2 -z-10 h-72 w-full max-w-2xl -translate-x-1/2 -translate-y-4 scale-150 scale-x-[2]  rounded-t-full bg-gradient-purple-blur transition-all"></div>
-
-        <div className="relative z-10 flex w-full justify-center">
-          <div className="relative">
-            <motion.div
-              variants={appear}
-              className="gap-2 text-center text-5xl font-semibold text-transparent sm:text-5xl md:flex md:text-6xl"
-            >
-              <h3 className="bg-gradient-title bg-clip-text text-transparent">
-                Some of
-              </h3>
-              <b className="subtitle-name inline-block pb-1 text-5xl font-semibold sm:text-5xl md:text-6xl">
-                {"  "}
-                my skills
-              </b>
-            </motion.div>
-            {/* <div className="absolute -left-4 top-2 z-20 h-[72px] w-56">
-              <div className="lines-left bg-gradient-to-b from-transparent !pb-[1.8px] !pl-0 !pr-[0px]">
-                <motion.div
-                  initial={{
-                    background:
-                      "conic-gradient(transparent,#a0f2 6%,#a0f 12%,#a0f 20%,#fff 22%, transparent 22.2%)",
-
-                    rotate: "0deg",
-                  }}
-                  animate={{
-                    background:
-                      "conic-gradient(transparent,#a0f2 6%,#a0f 12%,#a0f 20%,#fff 22%, transparent 22.2%)",
-
-                    rotate: "145deg",
-                    // transition: {
-                    //   duration: 2,
-                    //   // repeat: Infinity,
-                    //   ease: "linear",
-                    // },
-                  }}
-                  className="absolute -left-0 -top-32 h-60 w-60"
-                ></motion.div>
-              </div>
-            </div> */}
-            {/* <div className="absolute -left-0 top-2 z-20 h-[72px] w-56 -translate-x-full">
-              <div className="lines-left bg-gradient-to-b from-transparent !pb-[1.8px] !pl-0 !pr-[0px]">
-                <motion.div
-                  initial={{
-                    background:
-                      "conic-gradient(transparent 0.2%,#fff, #a0f 3.2%, #a0f 12.2%, #a0f7 20%, transparent 22.2%)",
-
-                    rotate: "0deg",
-                  }}
-                  animate={{
-                    background:
-                      "conic-gradient(transparent 0.2%,#fff, #a0f 3.2%, #a0f 12.2%, #a0f7 20%, transparent 22.2%)",
-
-                    rotate: "145deg",
-                    // transition: {
-                    //   duration: 2,
-                    //   // repeat: Infinity,
-                    //   ease: "linear",
-                    // },
-                  }}
-                  className="absolute -left-0 -top-32 h-60 w-60"
-                ></motion.div>
-              </div>
-            </div> */}
+        <motion.div variants={appear} className="flex flex-col gap-10">
+          <div className="relative z-10 flex w-full justify-center">
+            <div className="relative">
+              <motion.div className="gap-2 text-center text-5xl font-semibold text-transparent sm:text-5xl md:flex md:text-6xl">
+                <h3 className="bg-gradient-title bg-clip-text text-transparent">
+                  Some of
+                </h3>
+                <b className="subtitle-name inline-block pb-1 text-5xl font-semibold sm:text-5xl md:text-6xl">
+                  {"  "}
+                  my skills
+                </b>
+              </motion.div>
+            </div>
           </div>
-        </div>
-        <div className="relative h-[30rem] w-full">
-          <FirstRing />
-          <SecondRing />
-        </div>
+          <div className="relative mb-16 h-[30rem] w-full">
+            <FirstRing />
+            <SecondRing />
+          </div>
+          <div className="relative flex flex-col items-center justify-center gap-4">
+            <h5 className="bg-gradient-title bg-clip-text text-2xl font-semibold text-transparent">
+              I am currently learning
+            </h5>
+            <motion.div
+              variants={experienceLineContainer}
+              initial={"hidden"}
+              animate={"visible"}
+              className="relative flex h-auto w-auto items-center justify-center"
+            >
+              <div className="!relative -left-0 top-0 z-20 h-1 w-32 -translate-y-2 border-l border-t border-white/5">
+                <div className="lines-left bg-gradient-to-t from-transparent to-white/20 !p-[0px] !pt-[1.2px]">
+                  <motion.div
+                    variants={experienceLines}
+                    className="!absolute -left-4 -top-4 z-20 h-32 w-52 md:-left-10 md:top-0 md:h-32"
+                  ></motion.div>
+                </div>
+              </div>
+              <TechnologyItem
+                name="php"
+                svgImage="php.svg"
+                gradientColor="from-php"
+                className="relative object-cover"
+              />
+              <div className="!relative -left-0 top-0 z-20 h-1 w-32 -translate-y-2 border-l border-t border-white/5">
+                <div className="lines-left bg-gradient-to-t from-transparent to-white/20 !p-[0px] !pt-[1.2px]">
+                  <motion.div
+                    variants={experienceLinesReverse}
+                    className="!absolute -left-4 -top-0 z-20 h-32 w-52 md:-left-10 md:top-0 md:h-32"
+                  ></motion.div>
+                </div>
+              </div>
+            </motion.div>
+          </div>
+        </motion.div>
       </motion.div>
     </section>
   );
